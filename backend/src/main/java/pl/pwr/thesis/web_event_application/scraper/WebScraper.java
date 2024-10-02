b4c2d5939711d3957d0ff5431aac80ec7ad1a843
eden330 <fdutkiew@gmail.com>
2024-10-02 23:35:53 +0200

    Web scrapping algorithm implemented (mostly)

---
+++ backend/src/main/java/pl/pwr/thesis/web_event_application/scraper/WebScraper.java
@@ -0,0 +1,124 @@
+package pl.pwr.thesis.web_event_application.scraper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WebScraper {
+
+    // @Value("${SCRAPING_URL}")
+    private String scrapingUrl;
+    private final static String EVENT_DIV_HTML = "//div[@data-v-a4578fba" +
+            " and @data-v-7238e093 and .//div[contains(@class, 'column-inner')]]";
+    private final static String DOC_HEIGHT = "document.body.scrollHeight";
+
+    private final static int ATTEMPTS_NUMBER = 3;
+    private final Logger logger = Logger.getLogger(WebScraper.class.getName());
+
+    public Set<String> scrapEvents() {
+        WebDriverManager.chromedriver().setup();
+        logger.log(Level.INFO, "Web driver initialized");
+
+        ChromeOptions options = new ChromeOptions();
+        String[] optionsArray = new String[]{
+                "--headless=new", "--no-sandbox", "--disable-dev-shm-usage",
+                "--disable-gpu"
+        };
+        options.addArguments(optionsArray);
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get(scrapingUrl);
+
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+
+            scrollPage(driver, executor);
+
+            return scrapEventsJson(driver, executor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            logger.log(Level.INFO, "Web driver closed");
+            driver.quit();
+        }
+        return new HashSet<>();
+    }
+
+    private Set<String> scrapEventsJson(WebDriver driver, JavascriptExecutor executor) {
+        Set<String> eventsJsonSet = new LinkedHashSet<>();
+
+        List<WebElement> eventDivs = driver.findElements(
+                By.xpath(EVENT_DIV_HTML));
+
+        logger.log(Level.INFO, "Number of event divs read: " + eventDivs.size());
+
+        for (WebElement eventDiv : eventDivs) {
+            boolean isSuccess = false;
+            int retryCount = 0;
+
+            while (!isSuccess && retryCount < ATTEMPTS_NUMBER) {
+                try {
+                    executor.executeScript("arguments[0].scrollIntoView(true);", eventDiv);
+
+                    WebElement eventJsonElem = eventDiv.findElement(
+                            By.xpath(".//script[@type='application/ld+json']"));
+                    String eventJson = eventJsonElem.getAttribute("innerHTML");
+
+                    eventsJsonSet.add(eventJson);
+
+                    isSuccess = true;
+
+                } catch (StaleElementReferenceException e) {
+                    retryCount++;
+                    logger.log(Level.WARNING, "Stale element reference, retrying... (" + retryCount + "/3)");
+                    if (retryCount == 3) {
+                        logger.log(Level.SEVERE, "Failed to process element after retries, skipping element.");
+                    }
+                }
+            }
+        }
+        //eventsJsonSet.forEach(System.out::println);
+        logger.log(Level.INFO, "Number of json event elements: " + eventsJsonSet.size());
+
+        return eventsJsonSet;
+    }
+
+    private void scrollPage(WebDriver driver, JavascriptExecutor executor) {
+        while (true) {
+            int sizeBeforeScroll = driver.findElements(By.xpath(EVENT_DIV_HTML)).size();
+
+            executor.executeScript("window.scrollBy(0, " + DOC_HEIGHT + ")");
+
+            try {
+                new WebDriverWait(driver, Duration.ofSeconds(5)).until(driver1 -> {
+                    int sizeAfterScroll = driver.findElements(By.xpath(EVENT_DIV_HTML)).size();
+                    return sizeAfterScroll > sizeBeforeScroll;
+                });
+            } catch (TimeoutException e) {
+                logger.log(Level.WARNING, "No more events to load. Scrolling stopped!");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        WebScraper webScraper = new WebScraper();
+        webScraper.scrapEvents();
+    }
+}
